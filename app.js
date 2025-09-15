2782b074dae86fa34e0a71cb9d688caa54fd915b
ayan8653962 <ayan8653962@gmail.com>
2025-09-16 01:34:06 +0500

    day5

---
+++ app.js
@@ -0,0 +1,40 @@
+// question 1
+var math=(3 + 5)
+alert(math)
+// question 2
+var math1=+prompt()
+var math2=+prompt()
+var solove1=(math1 + math2)
+document.writeln("Addition")
+document.writeln(solove1) 
+
+var subtraction1=prompt()
+var subtraction2=prompt()
+document.writeln("Subtraction")
+var subsolve=(subtraction1 - subtraction2)
+document.writeln(subsolve)
+
+var multiplication1=prompt() 
+var multiplication2=prompt() 
+document.writeln("Multilipcation")
+var multsolve=(multiplication1 * multiplication2)
+document.writeln(multsolve)
+
+
+var division1=prompt() 
+var division2=prompt() 
+document.writeln("Division")
+var divisionsolve=(multiplication1 / multiplication2)
+document.writeln(divisionsolve)
+
+
+var module1=prompt() 
+var module2=prompt() 
+document.writeln("Module Us")
+var modulesolve=(module1 % module2)
+document.writeln(modulesolve)
+
+
+
+
+
