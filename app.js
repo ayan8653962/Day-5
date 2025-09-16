25f885bf97ab720d671c0eb2d493c23df88e51d0
ayan8653962 <ayan8653962@gmail.com>
2025-09-17 01:30:25 +0500

    day 5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // question 1
 var math=(3 + 5)
-alert(math)
+document.writeln(math)
 // question 2
 var math1=+prompt()
 var math2=+prompt()
@@ -33,6 +33,56 @@ var module2=prompt()
 document.writeln("Module Us")
 var modulesolve=(module1 % module2)
 document.writeln(modulesolve)
+
+// question 3
+var question3=(10)
+document.writeln(question3)
+var ques3solve=(++question3)
+document.writeln(ques3solve + " Increament of 1")
+var decrement=(11)
+document.writeln(decrement)
+var decrement1=(--decrement)
+document.writeln("decrement of " + decrement1)
+var moduleus=(decrement1 % 4)
+document.writeln("Module of " + moduleus)
+// question 4
+var tiket=(600)
+document.writeln("One Tiket Prize " + tiket)
+var tiket1=(tiket * 5)
+document.writeln("Total Tiket Prize " + tiket1)
+// question 5
+var ite1=prompt("Enter Item 1")
+var item1=(ite1)
+document.writeln(ite1)
+var ite2=prompt("Enter Item 2")
+var item2=(ite2)
+document.writeln(ite2)
+var ite3=prompt("Enter Item 1 Quantity 1")
+var quantity1=(ite3)
+document.writeln(ite3)
+var ite4=prompt("Enter Item 1 Quantity 2")
+var quantity2=(ite4)
+document.writeln(ite4)
+
+var shipping=(200)
+document.writeln("shipping Charges " + 200)
+
+var Total=(item1 + item2 + quantity1 + quantity2 + shipping)
+document.writeln(Total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
